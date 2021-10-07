faf3784b90ca39b380605dbdac514ef77a2aee72
Doug Davis <ddavis@ddavis.io>
2021-10-07 16:28:29 -0500

    General development; move top level `ak.` functions to a routines module. (#9)
    Fix up some typing

--- docs/examples/sandbox.py
+++ docs/examples/sandbox.py
@@ -4,7 +4,7 @@ import dask_awkward as dak  # noqa
 import dask_awkward.data as dakd
 
 
-daa = dakd.json_data(kind="numbers")
+daa = dak.from_json(dakd.json_data(kind="numbers"))
 aa = daa.compute()
 a0 = daa.partitions[0].compute()
 a1 = daa.partitions[1].compute()
