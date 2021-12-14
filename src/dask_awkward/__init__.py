25268e3610138e3b1e52bd0cbbbd1c3cd33e0ecb
Doug Davis <ddavis@ddavis.io>
2021-12-13 20:34:10 -0500

    remove routines.py; bring back in some reducer tests

--- src/dask_awkward/__init__.py
+++ src/dask_awkward/__init__.py
@@ -3,15 +3,6 @@ from .core import Array
 from .core import _type as type
 from .core import fields, from_awkward, map_partitions
 from .io import from_json
+from .reducers import *
 
 __version__ = version
-
-
-__all__ = (
-    "Array",
-    "fields",
-    "from_awkward",
-    "from_json",
-    "map_partitions",
-    "type",
-)
