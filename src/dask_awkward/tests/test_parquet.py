ed2e9255de0817d333d6d0d6b5ec5bdeb740622c
Martin Durant <martin.durant@alumni.utoronto.ca>
2022-01-20 15:24:24 -0500

    Add minimal parquet tests

---
+++ src/dask_awkward/tests/test_parquet.py
@@ -0,0 +1,61 @@
+import os
+import shutil
+
+import pytest
+
+import dask_awkward as dak
+
+
+@pytest.fixture(scope="module")
+def local_copy(tmpdir):
+    # could have done this by chaining "simplecache::" into the github URLs
+    tmpdir = str(tmpdir)
+    import fsspec
+
+    fs = fsspec.filesystem("github", org="scikit-hep", repo="awkward-1.0")
+    fs.get("tests/samples/*.parquet", tmpdir)
+    yield tmpdir
+    shutil.rmtree(tmpdir)
+
+
+def test_remote_single():
+    arr = dak.read_parquet(
+        "github://scikit-hep:awkward-1.0@/tests/samples/"
+        "nullable-record-primitives-simple.parquet"
+    )
+    assert arr.compute().to_list() == [
+        {"u4": None, "u8": 1},
+        {"u4": None, "u8": 2},
+        {"u4": None, "u8": 3},
+        {"u4": None, "u8": 4},
+        {"u4": None, "u8": 5},
+    ]
+
+
+def test_remote_double():
+    arr = dak.read_parquet(
+        [
+            "github://scikit-hep:awkward-1.0@/tests/samples/"
+            "nullable-record-primitives-simple.parquet",
+            "github://scikit-hep:awkward-1.0@/tests/samples/"
+            "nullable-record-primitives-simple.parquet",
+        ]
+    )
+    assert arr.npartitions == 2
+    assert (
+        arr.compute().to_list()
+        == [
+            {"u4": None, "u8": 1},
+            {"u4": None, "u8": 2},
+            {"u4": None, "u8": 3},
+            {"u4": None, "u8": 4},
+            {"u4": None, "u8": 5},
+        ]
+        * 2
+    )
+
+
+def test_fixture(local_copy):
+    files = os.listdir(local_copy)
+    assert files
+    assert all([s.endswith("parquet") for s in files])
