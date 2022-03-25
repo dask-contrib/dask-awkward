3761c030ed8374f4adc92fcd243207ef5ad1b224
Doug Davis <ddavis@anaconda.com>
2022-03-25 09:48:48 -0400

    use output_divisions arg for logic of divisions after map_partitions

--- src/dask_awkward/__init__.py
+++ src/dask_awkward/__init__.py
@@ -3,6 +3,7 @@ from dask_awkward import config  # isort:skip; load awkward config
 from dask_awkward._version import version
 from dask_awkward.core import Array, Record, Scalar
 from dask_awkward.core import _type as type
+from dask_awkward.core import map_partitions
 from dask_awkward.describe import fields
 from dask_awkward.io import (
     from_awkward,
