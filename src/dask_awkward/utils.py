a89f250f972a5b306c6482a5e0b6917b1c6c0478
Doug Davis <ddavis@ddavis.io>
2022-05-18 10:28:47 -0400

    organization

--- src/dask_awkward/utils.py
+++ src/dask_awkward/utils.py
@@ -32,6 +32,109 @@ class IncompatiblePartitions(ValueError):
         return msg
 
 
+class LazyInputsDict(collections.abc.Mapping):
+    """Dictionary with lazy key value pairs
+
+    Parameters
+    ----------
+    inputs : list[Any]
+        The list of dicionary values.
+
+    """
+
+    def __init__(self, inputs: list[Any], **kwargs: Any) -> None:
+        self.inputs = inputs
+        self.kwargs = kwargs
+
+    def __len__(self):
+        return len(self.inputs)
+
+    def __iter__(self):
+        return (self[k] for k in self.keys())
+
+    def __getitem__(self, i: tuple[int]) -> Any:
+        return self.inputs[i[0]]
+
+    def __contains__(self, k: Any):
+        if isinstance(k, tuple):
+            if isinstance(k[0], int):
+                return k[0] >= 0 and k[0] < len(self)
+        return False
+
+    def keys(self):
+        return ((i,) for i in range(len(self.inputs)))
+
+
+def borrow_docstring(original: Callable) -> Callable:
+    def wrapper(method):
+        method.__doc__ = (
+            f"Partitioned version of ak.{original.__name__}\n" f"{original.__doc__}"
+        )
+        return method
+
+    return wrapper
+
+
+def empty_typetracer() -> ak.Array:
+    """Instantiate a typetracer array with unknown length.
+
+    Returns
+    -------
+    ak.Array
+        Length-less typetracer array (content-less array).
+
+    """
+    a = ak.Array([])
+    return ak.Array(a.layout.typetracer.forget_length())
+
+
+def hyphenize(x: str) -> str:
+    """Replace underscores with hyphens.
+
+    Returns
+    -------
+    str
+        Resulting strings with hyphens replacing underscores.
+
+    """
+    return x.replace("_", "-")
+
+
+def is_empty_slice(s: Any) -> bool:
+    """Check if a slice is empty.
+
+    Parameters
+    ----------
+    s : Any
+        Slice of interest
+
+    Returns
+    -------
+    result : bool
+        True if the slice is empty
+
+    Examples
+    --------
+    >>> from dask_awkward.utils import is_empty_slice
+    >>> is_empty_slice(slice(1, 5, None))
+    False
+    >>> is_empty_slice(slice(None, None, 2))
+    False
+    >>> is_empty_slice(slice(None, None, None))
+    True
+
+    """
+    if not isinstance(s, slice):
+        return False
+    if s.start is not None:
+        return False
+    if s.stop is not None:
+        return False
+    if s.step is not None:
+        return False
+    return True
+
+
 def normalize_single_outer_inner_index(
     divisions: tuple[int, ...], index: int
 ) -> tuple[int, int]:
@@ -70,106 +173,3 @@ def normalize_single_outer_inner_index(
     partition_index = int(np.digitize(index, divisions)) - 1
     new_index = index - divisions[partition_index]
     return (partition_index, new_index)
-
-
-def is_empty_slice(s: Any) -> bool:
-    """Check if a slice is empty.
-
-    Parameters
-    ----------
-    s : Any
-        Slice of interest
-
-    Returns
-    -------
-    result : bool
-        True if the slice is empty
-
-    Examples
-    --------
-    >>> from dask_awkward.utils import is_empty_slice
-    >>> is_empty_slice(slice(1, 5, None))
-    False
-    >>> is_empty_slice(slice(None, None, 2))
-    False
-    >>> is_empty_slice(slice(None, None, None))
-    True
-
-    """
-    if not isinstance(s, slice):
-        return False
-    if s.start is not None:
-        return False
-    if s.stop is not None:
-        return False
-    if s.step is not None:
-        return False
-    return True
-
-
-def borrow_docstring(original: Callable) -> Callable:
-    def wrapper(method):
-        method.__doc__ = (
-            f"Partitioned version of ak.{original.__name__}\n" f"{original.__doc__}"
-        )
-        return method
-
-    return wrapper
-
-
-def empty_typetracer() -> ak.Array:
-    """Instantiate a typetracer array with unknown length.
-
-    Returns
-    -------
-    ak.Array
-        Length-less typetracer array (content-less array).
-
-    """
-    a = ak.Array([])
-    return ak.Array(a.layout.typetracer.forget_length())
-
-
-class LazyInputsDict(collections.abc.Mapping):
-    """Dictionary with lazy key value pairs
-
-    Parameters
-    ----------
-    inputs : list[Any]
-        The list of dicionary values.
-
-    """
-
-    def __init__(self, inputs: list[Any], **kwargs: Any) -> None:
-        self.inputs = inputs
-        self.kwargs = kwargs
-
-    def __len__(self):
-        return len(self.inputs)
-
-    def __iter__(self):
-        return (self[k] for k in self.keys())
-
-    def __getitem__(self, i: tuple[int]) -> Any:
-        return self.inputs[i[0]]
-
-    def __contains__(self, k: Any):
-        if isinstance(k, tuple):
-            if isinstance(k[0], int):
-                return k[0] >= 0 and k[0] < len(self)
-        return False
-
-    def keys(self):
-        return ((i,) for i in range(len(self.inputs)))
-
-
-def hyphenize(x: str) -> str:
-    """Replace underscores with hyphens.
-
-    Returns
-    -------
-    str
-        Resulting strings with hyphens replacing underscores.
-
-    """
-    return x.replace("_", "-")
