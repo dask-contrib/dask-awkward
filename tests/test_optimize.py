8fd57c64843a8e3a16a66419e881aab65263511f
Doug Davis <ddavis@anaconda.com>
2022-12-06 11:57:11 -0600

    improve optimization test; remove unncessary coverage file exclude

--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -7,6 +7,7 @@ import dask
 import dask_awkward as dak
 import dask_awkward.lib.optimize as o
 from dask_awkward.layers import AwkwardIOLayer
+from dask_awkward.lib.testutils import assert_eq
 
 
 def test_is_getitem(caa_parquet):
@@ -30,15 +31,15 @@ def test_requested_columns(caa_parquet):
             assert o._requested_columns_getitem(v) == {"x"}
 
 
-def test_config_adjust_1(caa_parquet):
-    from dask_awkward.lib import from_parquet
+@pytest.mark.parametrize("method", ["brute-force", "simple-getitem"])
+@pytest.mark.parametrize("nullmethod", [False, None, "none"])
+def test_config_adjust(caa_parquet, method, nullmethod):
+    with dask.config.set({"awkward.column-projection-optimization": nullmethod}):
+        a = dak.from_parquet([caa_parquet] * 3)
+        ref = a.points.x.compute()
 
-    a = from_parquet([caa_parquet] * 3)
-    with dask.config.set({"awkward.column-projection-optimization": "brute-force"}):
-        a.points.x.compute()
+    with dask.config.set({"awkward.column-projection-optimization": method}):
+        a = dak.from_parquet([caa_parquet] * 3)
+        opted = a.points.x.compute()
 
-
-def test_config_adjust_2(caa_parquet):
-    a = dak.from_parquet([caa_parquet] * 3)
-    with dask.config.set({"awkward.column-projection-optimization": "brute-force"}):
-        a.points.x.compute()
+    assert_eq(ref, opted, check_forms=False)
