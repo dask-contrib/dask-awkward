6a83f68bb06a44765825e0d4aaeed93794baae1e
Doug Davis <ddavis@anaconda.com>
2023-01-12 16:10:04 -0600

    some renaming in sizeof impl; use register_lazy

--- src/dask_awkward/sizeof.py
+++ src/dask_awkward/sizeof.py
@@ -1,6 +1,8 @@
-def ak_array_sizeof_plugin(sizeof):
-    import awkward as ak
+def register(sizeof):
+    @sizeof.register_lazy("awkward")
+    def lazy_sizeof_ak_array():
+        import awkward as ak
 
-    @sizeof.register(ak.Array)
-    def sizeof_ak_array(array):
-        return array.nbytes
+        @sizeof.register(ak.Array)
+        def sizeof_ak_array(array):
+            return array.nbytes
