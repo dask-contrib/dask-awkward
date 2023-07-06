bbf7d28e64a1e50ad1e6a395e2a11485f5b6f9c1
Doug Davis <ddavis@anaconda.com>
2023-07-06 16:58:37 -0500

    more sanity checks

--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -16,7 +16,14 @@ def test_multiple_computes(pq_points_dir) -> None:
     ds3 = dak.from_lists([[[1, 2, 3], [4, 5]], [[], [0, 0, 0]]])
 
     assert ds1.name != ds2.name
-    assert dask.compute(ds1.points.x, ds2.points.y)
-    assert dask.compute(ds1.points)
+    things1 = dask.compute(ds1.points.x, ds2.points.y)
+    things2 = dask.compute(ds1.points)
+    assert things2[0].x.tolist() == things1[0].tolist()
+
+    things3 = dask.compute(ds2.points.y, ds1.points.partitions[0])
+    assert things3[0].tolist() == things1[1].tolist()
+
+    assert len(things3[1]) < len(things3[0])
+
     things = dask.compute(ds1.points, ds2.points.x, ds2.points.y, ds1.points.y, ds3)
     assert things[-1].tolist() == ak.Array(lists[0] + lists[1]).tolist()
