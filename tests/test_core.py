75d65fd009223527d7ddf529c942136e46c543f2
Doug Davis <ddavis@anaconda.com>
2023-07-21 11:07:04 -0500

    actually let's fail it

--- tests/test_core.py
+++ tests/test_core.py
@@ -830,22 +830,11 @@ def test_map_partitions_no_dask_collections_passed(caa):
 
 
 @pytest.mark.parametrize("fn", [dak.count, dak.zeros_like, dak.ones_like])
-@pytest.mark.parametrize(
-    "opt_enabled",
-    [
-        pytest.param(
-            True, marks=pytest.mark.xfail(reason="these fail with opt enabled")
-        ),
-        False,
-    ],
-)
-def test_shape_only_ops(
-    fn: Callable, opt_enabled: bool, tmp_path_factory: pytest.TempPathFactory
-) -> None:
+def test_shape_only_ops(fn: Callable, tmp_path_factory: pytest.TempPathFactory) -> None:
     a = ak.Array([{"a": 1, "b": 2}, {"a": 3, "b": 4}])
     p = tmp_path_factory.mktemp("zeros-like-flat")
     ak.to_parquet(a, str(p / "file.parquet"))
     lazy = dak.from_parquet(str(p))
     result = fn(lazy.b)
-    with dask.config.set({"awkward.optimization.enabled": opt_enabled}):
+    with dask.config.set({"awkward.optimization.enabled": True}):
         result.compute()
