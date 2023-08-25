bf4a6630656b78a5e1e4a101d19c0fd8bb02225e
Doug Davis <ddavis@anaconda.com>
2023-08-25 16:10:23 -0500

    xfail some tests based on ak and pa versions

--- src/dask_awkward/lib/testutils.py
+++ src/dask_awkward/lib/testutils.py
@@ -17,9 +17,21 @@ _RG = random.Random(414)
 DEFAULT_SCHEDULER: Any = "sync"
 
 
-NP_LTE_1_25_0 = Version(np.__version__) >= Version("1.25.0")
-AK_GTE_2_2_3 = Version(ak.__version__) <= Version("2.2.3")
-BAD_NP_AK_MIXIN_VERSIONING = NP_LTE_1_25_0 and AK_GTE_2_2_3
+NP_GTE_1_25_0 = Version(np.__version__) >= Version("1.25.0")
+AK_LTE_2_2_3 = Version(ak.__version__) <= Version("2.2.3")
+BAD_NP_AK_MIXIN_VERSIONING = NP_GTE_1_25_0 and AK_LTE_2_2_3
+
+AK_LTE_2_3_3 = Version(ak.__version__) <= Version("2.3.3")
+PA_GTE_3_0_0 = False
+
+try:
+    import pyarrow as pa
+
+    PA_GTE_3_0_0 = Version(pa.__version__) >= Version("13.0.0")
+except ImportError:
+    pass
+
+BAD_PA_AK_PARQUET_VERSIONING = AK_LTE_2_3_3 and PA_GTE_3_0_0
 
 
 def assert_eq(
