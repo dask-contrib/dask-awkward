2e4b68027aa93a6ffcb8c167e26c305a2a80ea36
Doug Davis <ddavis@anaconda.com>
2023-11-09 13:03:58 -0600

    fix

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import random
 from pathlib import Path
 
 import awkward as ak
@@ -163,10 +162,10 @@ def test_from_map_exceptions() -> None:
         dak.from_map(f, [1, 2], [3, 4, 5])
 
     with pytest.raises(ValueError, match="must be `callable`"):
-        dak.from_map(5, [1], [2])  # type: ignore
+        dak.from_map(5, [1], [2])
 
     with pytest.raises(ValueError, match="must be Iterable"):
-        dak.from_map(f, 1, [1, 2])  # type: ignore
+        dak.from_map(f, 1, [1, 2])
 
     with pytest.raises(ValueError, match="non-zero length"):
         dak.from_map(f, [], [], [])
