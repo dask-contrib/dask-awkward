b9a1bab8245011b1c0d21208f2c791334adb1fab
Angus Hollands <goosey15@gmail.com>
2023-11-30 16:15:12 +0000

    test: check same and unalike cases

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -16,6 +16,57 @@ def test_concatenate_simple(daa, caa, axis):
     )
 
 
+def test_concatenate_axis_0_logical_same(daa):
+    result = dak.concatenate([daa, daa], axis=0)
+    print(daa.form)
+    buffers_report = dak.report_necessary_buffers(result.points.x)
+    assert len(buffers_report) == 1
+
+    buffers = next(iter(buffers_report.values()))
+
+    assert buffers.data_and_shape == frozenset(
+        ["@.points.content.x-data", "@.points-offsets"]
+    )
+    assert buffers.shape_only == frozenset()
+
+
+def test_concatenate_axis_0_logical_different(daa):
+    import dask.config
+
+    with dask.config.set(
+        {"awkward.optimization.on-fail": "raise", "awkward.raise-failed-meta": True}
+    ):
+        empty_form = ak.forms.from_dict(
+            {
+                "class": "RecordArray",
+                "fields": ["points"],
+                "contents": [
+                    {
+                        "class": "ListOffsetArray",
+                        "offsets": "i64",
+                        "content": {
+                            "class": "RecordArray",
+                            "fields": ["x", "y"],
+                            "contents": ["int64", "float64"],
+                        },
+                    }
+                ],
+            }
+        )
+        empty_array = ak.Array(empty_form.length_zero_array(highlevel=False))
+        empty_dak_array = dak.from_awkward(empty_array, npartitions=1)
+        result = dak.concatenate([daa, empty_dak_array], axis=0)
+
+        buffers_report = dak.report_necessary_buffers(result.points.x)
+        assert len(buffers_report) == 1
+
+        buffers = next(iter(buffers_report.values()))
+        assert buffers.data_and_shape == frozenset(
+            ["@.points.content.x-data", "@.points.content.y-data", "@.points-offsets"]
+        )
+        assert buffers.shape_only == frozenset()
+
+
 @pytest.mark.parametrize("axis", [0, 1, 2])
 def test_concatenate_more_axes(axis):
     a = [[[1, 2, 3], [], [100, 101], [12, 13]], [[1, 2, 3], [], [100, 101], [12, 13]]]
