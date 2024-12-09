29a7f1c3ef96b3648f2958f2ed4534c57f0b00c8
Lindsey Gray <lindsey.gray@gmail.com>
2024-12-09 13:54:00 -0600

    perf: go direct to _map_partitions in behavior functions

--- src/dask_awkward/lib/core.py
+++ src/dask_awkward/lib/core.py
@@ -1603,7 +1603,7 @@ class Array(DaskMethodsMixin, NDArrayOperatorsMixin):
 
                     @wraps(cls_method)
                     def wrapper(*args, **kwargs):
-                        return self.map_partitions(
+                        return self._map_partitions(
                             _BehaviorMethodFn(attr, **kwargs),
                             *args,
                             label=hyphenize(attr),
@@ -1611,7 +1611,7 @@ class Array(DaskMethodsMixin, NDArrayOperatorsMixin):
 
                     return wrapper
                 else:
-                    return self.map_partitions(
+                    return self._map_partitions(
                         _BehaviorPropertyFn(attr),
                         label=hyphenize(attr),
                     )
